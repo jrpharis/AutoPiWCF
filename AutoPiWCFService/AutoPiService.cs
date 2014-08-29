8ad2eaaf5d9007bbbda6c01da960e7eb2d6ab28a
Josh Pharis <josh.pharis@inin.com>
2014-08-28 23:42:08 -0400

    Initial Code commit

---
+++ AutoPiWCFService/AutoPiService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AutoPiWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class AutoPiService : IAutoPiService
+    {
+        public Dictionary<int, bool> GetLights()
+        {
+            return new Dictionary<int, bool>();
+        }
+
+        public KeyValuePair<int, bool> GetLightById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
